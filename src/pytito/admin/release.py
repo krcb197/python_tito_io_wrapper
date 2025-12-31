1f874a1a9cb013dc33f049183d1a5b06af6c11ff
krcb197 <34693973+krcb197@users.noreply.github.com>
2025-12-31 11:19:27 +0000

    Fix a bug with the None checks

--- src/pytito/admin/release.py
+++ src/pytito/admin/release.py
@@ -126,7 +126,7 @@ class Release(EventChildAPIBase):
         if value is None:
             self._update({'end_at': None})
         else:
-            if self.end_at is not None and value <= self.start_at:
+            if self.start_at is not None and value <= self.start_at:
                 raise ValueError(f'new end_at ({value}) is before the start_at ({self.end_at})')
             value_str = datetime_to_json(value)
             self._update({'end_at': value_str})
